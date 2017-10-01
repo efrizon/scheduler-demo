d76143340c7d6b384f075b6e52a0a404932270e4
efrizon <30643012+efrizon@users.noreply.github.com>
2017-09-30 23:09:30 -0300

    Programa finalizado

--- src/main.c
+++ src/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 int main() {
-  int a[5];
-  int n;
+	int a[5], n;
 
-  scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
+	scanf("%d %d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4], &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
-  }
-
-  return 0;
+	for (int i=1; i<=n; i++) {
+        int flag = 1;
+		for (int j=0; j<5; j++) {
+			if (i % a[j] == 0) printf ("%d", j), flag = 0;
+		}
+		if (flag) printf ("-");
+		printf ("\n");
+	}
+	return 0;
 }
